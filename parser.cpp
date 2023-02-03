bacf6f6e0957770cb9eb431c89878be4caaf69da
mMagicBean <50392507+mMagicBean@users.noreply.github.com>
2023-02-03 04:23:57 -0500

    Add files via upload

--- parser.cpp
+++ parser.cpp
@@ -5,75 +5,81 @@
 
 AST_Node* create_node() {
   AST_Node* _node = (AST_Node*) malloc(sizeof(AST_Node));
-  
-  _node->right = 0;
-  _node->left  = 0;
 
-  return _node;
+  if (_node) {
+    _node->value  = 0;
+    _node->right  = nullptr;
+    _node->left   = nullptr;
+    _node->parent = nullptr;
+    
+    return _node;
+  } else {
+    return nullptr;
+  }
 }
 
+void destroy_node(AST_Node* node) {
+  node->value  = NULL;
+  node->right  = NULL;
+  node->left   = NULL;
+  node->parent = NULL;
+  
+  free(node);
+}
+
+AST_Node* parse_expressions(std::vector<Token*> tokens, int index) {
+  if (tokens[index-1]->id == "integer_literal" && tokens[index+1]->id == "integer_literal") {
+    AST_Node* node1 = create_node();
+    AST_Node* node2 = create_node();
+    AST_Node* node3 = create_node();
+
+    node1->value = tokens[index]->value;
+    node2->value = tokens[index-1]->value;
+    node3->value = tokens[index+1]->value;
+
+    node1->left  = node2;
+    node1->right = node3;
+
+    node2->parent = node1;
+    node3->parent = node1;
+    
+    std::cout << "root_node: "<< node1->value << "first_node: " << node2->value << "second_node: " << node3->value << "\n";
+
+    return node1;
+  }
+  
+  return nullptr;
+}
 
 void parse_tokens(std::vector<Token*> tokens) {
-  AST_Node* root_node   = create_node(); 
-  AST_Node* first_node  = create_node();
-  AST_Node* second_node = create_node();
-  
-  AST_Node* nodes[3] = {root_node, first_node, second_node};
-  
   for (int i=0; i < tokens.size(); i++) {
-    if (tokens[i]->id == "operator") {
-      if (tokens[i-1]->id == "integer_literal" && tokens[i+1]->id == "integer_literal") {
-	
-	root_node->value = tokens[i]->value;     // *
-	first_node->value = tokens[i-1]->value;  // 2
-	second_node->value = tokens[i+1]->value; // 3
-	
-	root_node->left = first_node;           // * -> 2
-	root_node->right = second_node;         // |
-	                                        //  --> 3
+    if (tokens[i]->id == "operator" && tokens[i]->name == "*") {
+      parse_expressions(tokens, i);
+    }
+  }
+}
+
+void traverse_nodes(AST_Node* node) {
+  bool is_traversing = true;
+
+  AST_Node* curr_node = create_node();
+  AST_Node* next_node = node;
+
+  while(is_traversing) {
+    if (next_node->left != nullptr) {
+      next_node = next_node->left;
+    }
+
+    if (next_node->left == nullptr) {
+      next_node = next_node->parent;
+      
+      if (next_node->right != nullptr) {
+	next_node = next_node->right;
+		
+	if (next_node->right == nullptr) {
+	  is_traversing = false;
+	}
       }
-    } else {
-      continue;
-    }	 
-  }
-   
-  std::cout << "root_node: "<< root_node->value << "first_node: " << first_node->value << "second_node: " << second_node->value << "\n";
-}
-
-
-/*
-void parse_statement(std::vector<Token*> tokens) {
-  //std::cout << tokens[3]->name << "\n"; 
-  
-  if (tokens[0]->id != "keyword") {
-    std::cout << "Error: NO KEYWORD" << "\n";
-  } else {
-    std::cout << "KEYWORD PRESENT!" << "\n";
-  }
-  
-  if (tokens[1]->id != "keyword") {
-    std::cout << "Error: NO KEYWORD" << "\n";
-  } else {
-    std::cout << "KEYWORD PRESENT!" << "\n";
-  }
-
-  if (tokens[2]->id != "open_bracket") {
-    std::cout << "Error: Function missing open bracket" << "\n";
-  } else {
-    std::cout << "KEYWORD PRESENT!" << "\n";
-  }
-  
-  if (tokens[3]->name != "return" && tokens[3]->id != "keyword") {
-    std::cout << "main function is not void" << "\n";
-  } else {
-    std::cout << "RETURN PRESENT!" << "\n";
+    }
   }
 }
-
-
-void convert_operator_token(std::string op, int& a, int& b) {
-  if (op == "+") {
-    
-  }
-}
-*/
--- parser.h
+++ parser.h
@@ -5,11 +5,20 @@
 #include <vector>
 
 struct AST_Node {
-  int value = 0;
+  int value;
   
-  AST_Node* right = nullptr;
-  AST_Node* left  = nullptr;
+  AST_Node* right;
+  AST_Node* left;
+  AST_Node* parent;
 };
 
+struct AST {
+  AST_Node* nodes;
+};
+  
 AST_Node* create_node();
+void destroy_node(AST_Node* node);
+AST_Node* parse_expressions(std::vector<Token*> tokens, int index);
 void parse_tokens(std::vector<Token*> tokens); 
+void traverse_nodes(AST_Node* rnode);
+
